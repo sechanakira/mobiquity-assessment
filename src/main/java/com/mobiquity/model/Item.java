3deae48d5033b83143c5f21ab531b9fcc9ac8944
Shingirai Chanakira <sechanakira@yahoo.com>
2020-03-29 17:16:32 +0200

    Implementation

--- src/main/java/com/mobiquity/model/Item.java
+++ src/main/java/com/mobiquity/model/Item.java
@@ -1,9 +1,11 @@
 package com.mobiquity.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 @AllArgsConstructor
 public final class Item {
 
